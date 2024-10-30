61af5104a7c9a5ac929c47d38b5ce7c93fb12aae
Tatyana Tupalo <tatyana080489t@gmail.com>
2024-10-30 13:18:32 +0200

    add project srction

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import houseColor from "../images/house-color.jpg";
 import people from "../images/people.jpg";
 import peopleColor from "../images/people-color.jpg";
 import hero from "../images/hero.png";
-import menuBtton from "../images/menu-btn.svg";
-import closeBtn from "../images/close-btn.svg";
 import cube from "../images/cube.png";
 import development from "../images/development.png";
 import execution from "../images/execution.png";
 import monitoring from "../images/monitoring.png";
 import planing from "../images/planing.png";
 import person from "../images/person.png";
+import sneakersColor from "../images/sneakers-color.jpg";
+import arrow from "../images/arrow.png";
 
 const menuBtn = document.querySelector(".header__burger");
 const menuBody = document.querySelector(".header__menu");

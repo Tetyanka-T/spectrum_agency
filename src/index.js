f5786f63e5bd2ad43d8d6232a5c5fd480c72cadd
Tatyana Tupalo <tatyana080489t@gmail.com>
2024-11-04 11:54:01 +0200

    fix human section add form section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./sass/main.scss";
-import bgPerson from "../images/bg-person.png";
 import bottle from "../images/bottle.jpg";
 import bottleColor from "../images/bottle-color.jpg";
 import house from "../images/house.jpg";
@@ -12,9 +11,10 @@ import development from "../images/development.png";
 import execution from "../images/execution.png";
 import monitoring from "../images/monitoring.png";
 import planing from "../images/planing.png";
-import person from "../images/person.png";
+import sneakers from "../images/sneakers.jpg";
 import sneakersColor from "../images/sneakers-color.jpg";
 import arrow from "../images/arrow.png";
+import human from "../images/human.jpg";
 
 const menuBtn = document.querySelector(".header__burger");
 const menuBody = document.querySelector(".header__menu");
